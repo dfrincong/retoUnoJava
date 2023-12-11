416b2393f2f249d43a56c61077630cdc07cdce78
dfrincong <dfrincong111@gmail.com>
2023-12-11 10:58:22 -0500

    Se crea proyecto de reto 1 java

---
+++ Reto1Java/src/main/java/com/mycompany/reto1java/Punto3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.reto1java;
+
+/**
+ *
+ * @author user
+ */
+public class Punto3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
