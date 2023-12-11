3fd00b7cf99155a8453e5351f4509e0769527a41
dfrincong <dfrincong111@gmail.com>
2023-12-11 12:09:50 -0500

    Se agrega solución punto 2

--- Reto1Java/src/main/java/com/mycompany/reto1java/Punto2.java
+++ Reto1Java/src/main/java/com/mycompany/reto1java/Punto2.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.reto1java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -14,7 +16,41 @@ public class Punto2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese una contraseña: ");
+        String contraseña = sc.nextLine();
+        
+        int mayus = 0;
+        int minus = 0;
+        int number = 0;
+        int special = 0;
+        
+        if (contraseña.length() >= 8) {
+            for (int i = 0; i < contraseña.length(); i++) {
+                if (contraseña.charAt(i) >= 65 && contraseña.charAt(i) <= 90) {
+                    mayus += 1;
+                }
+                if (contraseña.charAt(i) >= 97 && contraseña.charAt(i) <= 122) {
+                    minus += 1;
+                }
+                if (contraseña.charAt(i) >= 48 && contraseña.charAt(i) <= 57) {
+                    number += 1;
+                }
+                if (contraseña.charAt(i) == 33 || contraseña.charAt(i) == 64 || contraseña.charAt(i) == 35 || contraseña.charAt(i) == 36 || contraseña.charAt(i) == 37 || contraseña.charAt(i) == 94 || contraseña.charAt(i) == 38 || contraseña.charAt(i) == 42) {
+                    special += 1;
+                }
+                
+            }
+            if (mayus >= 1 && minus >= 1 && number >= 1 && special >=1) {
+                System.out.println("Contraseña valida.");
+            } else {
+                System.out.println("Contraseña no valida, la contraseña debe tener al menos una letra mayúscula, una letra minúscula, un número y uno de los siguientes caracteres especiales (!, @, #, $, %, ^, &, *).");
+            }
+        } else {
+            System.out.println("Contraseña no valida, la contraseña debe tener mínimo 8 caracteres.");
+        }
     }
     
 }
