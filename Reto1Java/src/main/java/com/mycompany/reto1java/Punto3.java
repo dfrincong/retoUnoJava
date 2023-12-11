5a3e038cae5f576a7c2ab972e625bb73193616f0
dfrincong <dfrincong111@gmail.com>
2023-12-11 13:43:48 -0500

    Se agrega punto 3

--- Reto1Java/src/main/java/com/mycompany/reto1java/Punto3.java
+++ Reto1Java/src/main/java/com/mycompany/reto1java/Punto3.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.reto1java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -14,7 +16,40 @@ public class Punto3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String contraseñaAleatoria = "";
+        String minus = "abcdefghijklmnopqrstuvwxyz";
+        String mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numero = "1234567890";
+        String special = "!#$%&/=@";
+        
+        System.out.print("Ingrese el largo de la contraseña (entre 8 y 16 caracteres): ");
+        int largoContraseña = sc.nextInt();
+        
+        System.out.println("Quiere letras mayúsculas (Responda y para sí o n para no):");
+        String resUno = sc.nextLine();
+        
+        System.out.println("Quiere números (Responda y para sí o n para no):");
+        String resDos = sc.nextLine();
+        
+        System.out.println("Quiere símbolos (Responda y para sí o n para no):");
+        String resTres = sc.nextLine();
+        
+        for (int i = 0; i < largoContraseña; i++) {
+            if ((resUno  == "Y" || resUno == "y") && (resDos  == "y" || resDos == "Y") && (resTres  == "y" || resTres == "Y")) {
+//                contraseñaAleatoria.charAt(i) == ;
+            } else if ((resUno  == "Y" || resUno == "y") && (resDos  == "n" || resDos == "N") && (resTres  == "n" || resTres == "N")) {
+//                contraseñaAleatoria.charAt(i) == ;
+            }
+        }
+        
+        
+        
+        
+        
+        
     }
     
 }
